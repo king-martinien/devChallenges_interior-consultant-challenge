a91450998f4694989e39b6cf2d7f5bb38e67b1ab
King-Martinien <martinienfokoue@yahoo.com>
2022-01-02 18:13:44 +0100

    adding style with scss and mobile first

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+let hamburger = document.querySelector(".hamburger");
+let navigation = document.querySelector(".navigation");
+
+(() => {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    navigation.classList.toggle("is-active");
+  });
+})();
